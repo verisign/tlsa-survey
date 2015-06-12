afa244e9629a3a4e2655e00f39d67aac82931ab4
Antonio Prado <antonio@prado.it>
2015-06-12 17:18:15 +0200

    Update tlsa_survey.sh

--- tlsa_survey.sh
+++ tlsa_survey.sh
@@ -42,11 +42,12 @@ DATA_DIR="data"
 mkdir -p $DATA_DIR
 DB="$DATA_DIR/stats.$DATE.db"
 THREAD_NUM="20"
+NSSERVER=$2
 
 # script to get server certs using openssl
 CERT_SCRIPT="$SCRIPT_DIR/get_serv_cert.sh"
 
-cat $INPUT | python $SCRIPT_DIR/tlsa_survey.py -d -s 127.0.0.1:53 -t $THREAD_NUM -o $DB -c $CERT_SCRIPT
+cat $INPUT | python $SCRIPT_DIR/tlsa_survey.py -d -s $NSSERVER:53 -t $THREAD_NUM -o $DB -c $CERT_SCRIPT
 
 # get tlsa zone and dnssec zone number
 python $SCRIPT_DIR/dnssec_tlsa_zone_num.py -i $DATA_DIR/dnssec_tlsa_zone_num.db -d $DATE -n 0 -p $SCRIPT_DIR
